7445a743ac03f0da9b550899caa588cdcbe49147
DawidMatura <72615110+dawimat009@users.noreply.github.com>
2022-04-05 10:49:56 +0200

    Testy 1

---
+++ ProjektBiblioteka/ProjektTestowy/MockKsiazkaRepo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using ProjektBiblioteka.Repositories;
+using ProjektBiblioteka.Models;
+namespace ProjektTestowy
+{
+    class MockKsiazkaRepo : Mock<IKsiazkaRepo>
+    {
+        public MockKsiazkaRepo MockGetKsiazkaInvalid(Ksiazka wynik)
+        {
+            Setup(x => x.GetBook(It.IsAny<int>())).Returns(wynik);
+            return this;
+        }
+        public MockKsiazkaRepo MockGetKsiazka(Times times)
+        {
+            Verify(x => x.GetBook(It.IsAny<int>()), times);
+
+            return this;
+        }
+        public MockKsiazkaRepo MockForBooks(List<Ksiazka> results)
+        {
+            Setup(x => x.GetBooks())
+                .Returns(results);
+
+            return this;
+        }
+    }
+}
---
+++ ProjektBiblioteka/ProjektTestowy/UnitTest1.cs
@@ -0,0 +1,15 @@
+using ProjektBiblioteka.Repositories;
+using ProjektBiblioteka.Models;
+using System;
+using Xunit;
+namespace ProjektTestowy
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            
+        }
+    }
+}
