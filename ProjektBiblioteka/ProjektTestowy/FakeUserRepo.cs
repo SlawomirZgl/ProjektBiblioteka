ab082df2b4d4bdc9cd851075df3bee5f65711f57
DawidMatura <72615110+dawimat009@users.noreply.github.com>
2022-04-06 09:53:12 +0200

    testy2

---
+++ ProjektBiblioteka/ProjektTestowy/FakeUserRepo.cs
@@ -0,0 +1,56 @@
+﻿using ProjektBiblioteka.Models;
+using ProjektBiblioteka.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjektTestowy
+{
+    class FakeUserRepo : IUzytkownikRepo
+    {
+        private List<Uzytkownik> _users = new List<Uzytkownik>();
+        public void AddKsiazkatoUser(Ksiazka ksiazka, int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddUser(Uzytkownik uzytkownik)
+        {
+            _users.Add(uzytkownik);
+        }
+
+        public bool DeleteUser(int id)
+        {
+            bool temp1 = false;
+            Uzytkownik temp = _users.Where(u => u.UzytkownikId == id).FirstOrDefault();
+            if(temp != null)
+            {
+                temp1 = true;
+                _users.Remove(temp);
+            }
+            return temp1;
+        }
+
+        public IEnumerable<Ksiazka> GetBooksOfUser(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Uzytkownik GetUser(int id)
+        {
+            return _users.Where(u => u.UzytkownikId == id).FirstOrDefault();
+        }
+
+        public IEnumerable<Uzytkownik> GetUsers()
+        {
+            return _users;
+        }
+
+        public void RemoveBookFromUser(Ksiazka ksiazka, int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- ProjektBiblioteka/ProjektTestowy/UnitTest1.cs
+++ ProjektBiblioteka/ProjektTestowy/UnitTest1.cs
@@ -2,14 +2,89 @@ using ProjektBiblioteka.Repositories;
 using ProjektBiblioteka.Models;
 using System;
 using Xunit;
+using System.Collections.Generic;
+using Moq;
+using NUnit.Framework;
+using FluentAssertions;
 namespace ProjektTestowy
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Test]
+        public void Fake()
         {
-            
+            var userRepo = new FakeUserRepo();
+            userRepo.AddUser(GetUzytkownicy()[0]);
+            userRepo.AddUser(GetUzytkownicy()[1]);
+            userRepo.AddUser(GetUzytkownicy()[2]);
+            string nazwa = userRepo.GetUser(1).Nazwa;
+            nazwa.Should()
+                .Be("Maciek");
+
+        }
+        [Fact]
+        public void TestMockKsiazka()
+        {
+            var mockksiazki = GetKsiazki();
+            var mockKsiazkaRepository = new MockKsiazkaRepo().MockForBooks(mockksiazki);
+            mockKsiazkaRepository.MockGetKsiazka(Times.Once());
+        }
+        private List<Ksiazka> GetKsiazki()
+        {
+            return new List<Ksiazka>()
+        {
+            new Ksiazka()
+            {
+                KsiazkaID =1,
+                Ilosc = 3,
+                Nazwa = "W pustyni i w puszczy",
+                Ocena = 3,
+                Opis = " aa"
+            },
+            new Ksiazka()
+            {
+                KsiazkaID =2,
+                Ilosc = 2,
+                Nazwa = "W pustyni i w puszczy2",
+                Ocena = 3,
+                Opis = " ba"
+            },
+            new Ksiazka()
+            {
+                KsiazkaID =3,
+                Ilosc = 3,
+                Nazwa = "W pustyni i w puszczy3",
+                Ocena = 3,
+                Opis = " ca"
+            },
+        };
+
+        }
+        private List<Uzytkownik> GetUzytkownicy()
+        {
+            return new List<Uzytkownik>()
+            {
+                new Uzytkownik()
+                {
+                    UzytkownikId = 1,
+                    Nazwa = " Maciek",
+                    Haslo = "Dobre"
+                },
+                 new Uzytkownik()
+                {
+                    UzytkownikId = 2,
+                    Nazwa = " Maciek2",
+                    Haslo = "Dobre2"
+                },
+                  new Uzytkownik()
+                {
+                    UzytkownikId = 3,
+                    Nazwa = " Maciek2",
+                    Haslo = "Dobre2"
+                }
+
+            };
         }
     }
+    
 }
